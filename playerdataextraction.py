67db849f41f472d49047c175cde603a70eaead52
Ruwindhu Dilanga Chandraratne Hettige Don <ruwindhu123@gmail.com>
2023-07-13 00:23:21 -0400

    Playerdataextraction performed

---
+++ playerdataextraction.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import requests
+import time
+years = list(range(1977,2006))
+
+url_s = " https://www.basketball-reference.com/leagues/NBA_{}_per_game.html "
+
+for year in years:
+    url = url_s.format(year)
+    data = requests.get(url)
+    with open("players/{}.html".format(year),"w+",encoding="utf-8") as f:
+        f.write(data.text)
+
