76d9695ac8dd2f2648bd98c0ffa375654ede6596
Ruwindhu Dilanga Chandraratne Hettige Don <ruwindhu123@gmail.com>
2023-07-13 00:10:17 -0400

    Rookie of the year parser and dataset creation

---
+++ dataparser_dopy.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+
+years = list(range(2022,2023))
+dfs = []
+for year in years:
+    with open("awards/{}.html".format(year),encoding="utf-8") as f: 
+        page = f.read()
+    soup = BeautifulSoup(page,"html.parser")
+    soup.find('tr',class_="over_header").decompose()
+    dpoy_data = soup.find(id="dpoy")
+
+    dpoy_table = pd.read_html(str(dpoy_data))[0]
+    dfs.append(dpoy_table)
+    
+dpoy_data = pd.concat(dfs)
+
+dpoy_data.to_csv("dpoy_1983on.csv",index=False)
+
---
+++ dataparser_roty.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+
+years = list(range(1977,2023))
+dfs = []
+for year in years:
+    with open("awards/{}.html".format(year),encoding="utf-8") as f: 
+        page = f.read()
+    soup = BeautifulSoup(page,"html.parser")
+    soup.find('tr',class_="over_header").decompose()
+    roy_data = soup.find(id="roy")
+
+    roy_table = pd.read_html(str(roy_data))[0]
+    dfs.append(roy_table)
+    
+roy_data = pd.concat(dfs)
+
+roy_data.to_csv("roy_1976on.csv",index=False)
+
